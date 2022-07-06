550f92139cdff892de49269eca10ae5406cfdeda
Gabriel-Mugge <52550202+Gabriel-Mugge@users.noreply.github.com>
2022-07-06 12:07:45 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='conector_sharepoint',
+version='0.1',
+description='Testing installation of Package',
+url='',
+author='Gabriel-Mugge',
+author_email='',
+license='',
+packages=['ConectorSharepoint'],
+zip_safe=False)
