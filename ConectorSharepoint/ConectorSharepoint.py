42640cbb55d223f10cc396761836a287dcfa915f
Gabriel-Mugge <52550202+Gabriel-Mugge@users.noreply.github.com>
2022-07-08 14:46:04 -0300

    Update ConectorSharepoint.py

--- ConectorSharepoint/ConectorSharepoint.py
+++ ConectorSharepoint/ConectorSharepoint.py
@@ -152,7 +152,7 @@ class ConectorSharepoint():
       return
 
   def call_the_mika(self):
-    img = mpimg.imread('SharepointConector/ConectorSharepoint/egg/sol.jpg')
+    img = mpimg.imread('sol.jpg')
     imgplot = plt.imshow(img)
     plt.show()
     plt.close()
